22e9f604db5e0e0f0c1e852c3affbd09238f728c
Dominic Tremblay <dominictremblay24@gmail.com>
2019-09-03 23:57:41 -0400

    ready for demo

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 const express = require('express');
 const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-let cookieSession = require('cookie-session');
 const methodOverride = require('method-override');
-const bcrypt = require('bcryptjs');
 const uuid = require('uuid/v4');
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-// app.use(cookieParser());
-app.use(
-  cookieSession({
-    name: 'session',
-    keys: ['3326ec32-a41c-41e6-8619-cbafe93cf39b', '52329e8c-5082-4f95-911a-7d1da76c7c7f'],
-  })
-);
+
 app.use(methodOverride('_method'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Creating a middleware to check if there is a currently logged in user
-const currentUser = (req, res, next) => {
-  // reading the cookie from the request
-  const userId = req.session.user_id;
-
-  console.log('Current user:');
-  console.log({ userId }, users[userId]);
-
-  // Attach the user object to the request if it exits
-  req.user = users[userId] || null;
-
-  // call the next middleware
-  next();
-};
-
-app.use(currentUser);
 
 const quotesDb = {
   'd9424e04-9df6-4b76-86cc-9069ca8ee4bb': {
@@ -97,85 +71,8 @@ const quoteList = () => {
   return quotes;
 };
 
-const findUser = email => Object.values(users).find(user => user.email === email);
-
-const addNewUser = (name, email, password) => {
-  const id = uuid().substr(0, 8);
-
-  users[id] = {
-    id,
-    name,
-    email,
-    password,
-  };
-
-  return id;
-};
-
-const authenticateUser = (email, password) => {
-  // does the user exists
-  const user = findUser(email);
-
-  if (user && bcrypt.compareSync(password, user.password)) {
-    return user;
-  } else {
-    return false;
-  }
-};
-
 app.get('/users', (req, res) => {
   res.json(users);
-});
-
-app.get('/register', (req, res) => {
-  const templateVars = { currentUser: req.user };
-  res.render('register', templateVars);
-});
-
-app.post('/users', (req, res) => {
-  // extract info from the request body
-  const { name, email, password } = req.body;
-
-  // Find out if the user exists
-  const user = findUser(email);
-
-  // Add a new user to the db, unless the user exists
-  if (user) {
-    res.status(401).send('A user with that email already exists. Try to login.');
-  } else {
-    //encrypt the password
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(password, salt, function(err, encryptedPassword) {
-        // Store hash in your password DB.
-        // create the user in the database
-        const userId = addNewUser(name, email, encryptedPassword);
-        // set the user id in the cookie
-        req.session.user_id = userId;
-        // redirect to the list of quotes
-        res.redirect('/quotes');
-      });
-    });
-  }
-});
-
-app.get('/login', (req, res) => {
-  const templateVars = { currentUser: req.user };
-  res.render('login', templateVars);
-});
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  // authenticate the user
-  const user = authenticateUser(email, password);
-
-  if (user) {
-    // log the user in
-    req.session.user_id = user.id;
-    res.redirect('/quotes');
-  } else {
-    res.status(401).send('Wrong email or password');
-  }
 });
 
 app.get('/', (req, res) => res.redirect('/quotes'));
@@ -184,11 +81,6 @@ app.get('/quotes', (req, res) => {
   const quotes = Object.values(quoteList());
 
   res.render('quotes', { quotes, currentUser: req.user });
-});
-
-app.delete('/login', (req, res) => {
-  req.session = null;
-  res.redirect('/login');
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
